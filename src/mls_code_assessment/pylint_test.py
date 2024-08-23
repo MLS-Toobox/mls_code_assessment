f7acea4395196d4e69ecdd9167e2d331d2432324
MikoMikarro <lopezcuestam@gmail.com>
2024-08-23 12:52:44 +0200

    fixed path

--- src/mls_code_assessment/pylint_test.py
+++ src/mls_code_assessment/pylint_test.py
@@ -14,15 +14,16 @@ class PyLintTest(CodeAssess):
     def rate_app(self):
         files = os.listdir(self.local_path)
         json_output = {}
-
+        pre_dir = os.getcwd()
         for file in files:
             # check file is folder
-            if os.path.isdir(self.local_path + '/' + file):
-                current_dir = os.getcwd() + '/' + self.session_id + '/' + file
+            if os.path.isdir(os.path.join(self.local_path, file)):
+                current_dir = os.path.join(pre_dir, self.session_id, file)
                 try:
+                    os.chdir(current_dir)
                     subprocess.check_output(
                         ["pylint", "--recursive", "y", "--output-format", "json2",
-                         "--disable", "E0401", "--clear-cache-post-run", "y", current_dir])
+                         "--disable", "E0401", "--clear-cache-post-run", "y", '.'])
                 except subprocess.CalledProcessError as e:
                     json_output = json.loads(str(e.output, "utf-8"))
 
@@ -31,19 +32,24 @@ class PyLintTest(CodeAssess):
             "message_count": json_output["statistics"]["messageTypeCount"],
             "module_count": json_output["statistics"]["modulesLinted"]
         }
+        os.chdir(pre_dir)
 
     def run_report(self):
         files = os.listdir(self.local_path)
         readable_output = None
 
+        pre_dir = os.getcwd()
         for file in files:
             # check file is folder
-            if os.path.isdir(self.local_path + '/' + file):
-                current_dir = os.getcwd() + '/' + self.session_id + '/' + file
+            if os.path.isdir(os.path.join(self.local_path, file)):
+                current_dir = os.path.join(pre_dir, self.session_id, file)
                 try:
+                    os.chdir(current_dir)
                     subprocess.check_output(
                         ["pylint", "--recursive", "y", "--score","n", "--reports", "y",
-                         "--disable", "E0401", "--clear-cache-post-run", "y", current_dir])
+                         "--disable", "E0401", "--clear-cache-post-run", "y", '.'])
                 except subprocess.CalledProcessError as e:
                     readable_output = e.output
         self.full_report = readable_output
+        os.chdir(pre_dir)
+        
