14694615f26e4cc0109d93400844ed50875e18d4
MikoMikarro <lopezcuestam@gmail.com>
2024-08-24 14:02:43 +0200

    .

--- src/mls_code_assessment/test_list.py
+++ src/mls_code_assessment/test_list.py
@@ -1,3 +1,4 @@
 from . pylint_test import PyLintTest
-
-TESTS = [PyLintTest]
+from . radon_cc_test import RadonCCTest
+from . radon_mi_test import RadonMITest
+TESTS = [ PyLintTest, RadonMITest, RadonCCTest]
